4ff413f4c4c3ae662bf2271bd334b8b77e058c76
syd711 <syd711@googlemail.com>
2015-01-11 21:13:41 +0100

    Added new shift register

--- callete-api-impl/src/main/java/callete/api/services/impl/gpio/raspi/PiShiftRegister74hc595.java
+++ callete-api-impl/src/main/java/callete/api/services/impl/gpio/raspi/PiShiftRegister74hc595.java
@@ -5,13 +5,16 @@ import callete.api.services.gpio.DigitalOutputPin;
 import callete.api.services.gpio.GPIOService;
 import callete.api.services.gpio.PinState;
 import callete.api.services.gpio.ShiftRegister74hc595;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * The 8-bit shift register implementation using the pi GPIO API
  */
 public class PiShiftRegister74hc595 implements ShiftRegister74hc595 {
+  private final static Logger LOG = LoggerFactory.getLogger(PiShiftRegister74hc595.class);
   private String name;
-  
+
   private int serPin;
   private int rclkPin;
   private int srclkPin;
@@ -28,10 +31,10 @@ public class PiShiftRegister74hc595 implements ShiftRegister74hc595 {
     this.serPin = serPin;
     this.rclkPin = rclkPin;
     this.srclkPin = srclkPin;
-    
-    int numOfRegisterPins = chips*8;
+
+    int numOfRegisterPins = chips * 8;
     registerStates = new PinState[numOfRegisterPins];
-    
+
     connect();
   }
 
@@ -42,24 +45,25 @@ public class PiShiftRegister74hc595 implements ShiftRegister74hc595 {
 
   @Override
   public void clearRegisters() {
-    for(int i=0; i< registerStates.length; i++) {
+    for(int i = 0; i < registerStates.length; i++) {
       this.registerStates[i] = PinState.LOW;
     }
   }
 
   @Override
   public void writeRegisters() {
+    LOG.info("Updating registers of " + this);
     rclk.low();
-    
-    for(int i = registerStates.length - 1; i >=  0; i--){
+
+    for(int i = registerStates.length - 1; i >= 0; i--) {
       srclk.low();
 
       PinState registerState = registerStates[i];
       ser.setState(registerState);
-      
+
       srclk.high();
     }
-    
+
     rclk.high();
   }
 
@@ -68,12 +72,21 @@ public class PiShiftRegister74hc595 implements ShiftRegister74hc595 {
     return name;
   }
 
+  @Override
+  public String toString() {
+    return "ShiftRegister '" + name + "'";
+  }
+
   //--------------------------- Helper---------------------------------------------------
-  
+
   private void connect() {
+    LOG.info("Connecting " + this);
     GPIOService gpioService = Callete.getGPIOService();
+    LOG.info("Connecting SER pin");
     ser = gpioService.connectDigitalOutputPin(serPin, "SER", PinState.LOW);
+    LOG.info("Connecting RCLK pin");
     rclk = gpioService.connectDigitalOutputPin(rclkPin, "RCLK", PinState.LOW);
+    LOG.info("Connecting SRCLK pin");
     srclk = gpioService.connectDigitalOutputPin(srclkPin, "SRCLK", PinState.LOW);
   }
 }
