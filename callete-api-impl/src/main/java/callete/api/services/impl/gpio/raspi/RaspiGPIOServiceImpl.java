b5163fc470924c176853a2b31326d222d6ddb590
Matthias Faust <syd711@gmail.com>
2022-07-14 22:11:31 +0200

    qa

--- callete-api-impl/src/main/java/callete/api/services/impl/gpio/raspi/RaspiGPIOServiceImpl.java
+++ callete-api-impl/src/main/java/callete/api/services/impl/gpio/raspi/RaspiGPIOServiceImpl.java
@@ -12,17 +12,18 @@ import org.slf4j.LoggerFactory;
  */
 public class RaspiGPIOServiceImpl {
   private final static Logger LOG = LoggerFactory.getLogger(GPIOServiceImpl.class);
-  
+
   private final static String RASPBERRY_PI_MODEL_B = "Raspberry Pi Model B";
   private final static String RASPBERRY_PI_2_MODEL_B = "Raspberry Pi 2 Model B";
-  
+  private final static String RASPBERRY_PI_3_MODEL_B = "Raspberry Pi 3 Model B";
+
   private String board;
 
   public PiShiftRegister74hc595 connectShiftRegister(int chips, int serPin, int rclkPin, int srclkPin, String name) {
-    LOG.info("Creating shift register '" + name + "' using pins " + serPin + ", " + rclkPin + ", "+  srclkPin);
+    LOG.info("Creating shift register '" + name + "' using pins " + serPin + ", " + rclkPin + ", " + srclkPin);
     return new PiShiftRegister74hc595(chips, serPin, rclkPin, srclkPin, name);
   }
-  
+
   public PushButton connectPushButton(int pin, String name) {
     LOG.info("Creating push button for pin " + pin);
     return new PiPushButton(pin, name);
@@ -39,43 +40,37 @@ public class RaspiGPIOServiceImpl {
   }
 
   public Object convertPinStateToApiInstance(PinState state) {
-    if(state.equals(PinState.HIGH)) {
+    if (state.equals(PinState.HIGH)) {
       return com.pi4j.io.gpio.PinState.HIGH;
     }
     return com.pi4j.io.gpio.PinState.LOW;
   }
 
   public Object convertPinToApiInstance(int pin) {
-    if(board == null) {
+    if (board == null) {
       board = Callete.getConfiguration().getString("gpio.board");
-      if(board == null) {
-        LOG.warn("No 'gpio.board' configuration found, applying pin of " + RASPBERRY_PI_MODEL_B + " as default.");
-        board = RASPBERRY_PI_MODEL_B;
+      if (board == null) {
+        LOG.warn("No 'gpio.board' configuration found, applying pin of " + RASPBERRY_PI_3_MODEL_B + " as default.");
+        board = RASPBERRY_PI_3_MODEL_B;
       }
-      
-      if(board.equals(RASPBERRY_PI_MODEL_B)) {
-        LOG.info("Using GPIO configuration for board " + RASPBERRY_PI_MODEL_B);
-      }
-      else if(board.equals(RASPBERRY_PI_2_MODEL_B)) {
-        LOG.info("Using GPIO configuration for board " + RASPBERRY_PI_2_MODEL_B);
-      }
-      else {
-        LOG.error("No valid board definition '" + board + "', appyling " + RASPBERRY_PI_MODEL_B + " instead.");
-        board = RASPBERRY_PI_MODEL_B;
-      }
+
+      LOG.info("Using GPIO configuration for board " + RASPBERRY_PI_MODEL_B);
     }
 
-    if(board.equals(RASPBERRY_PI_MODEL_B)) {
+    if (board.equals(RASPBERRY_PI_MODEL_B)) {
       return convert2RaspberryPiModelBPin(pin);
     }
-    else if(board.equals(RASPBERRY_PI_2_MODEL_B)) {
+    else if (board.equals(RASPBERRY_PI_2_MODEL_B)) {
+      return convert2RaspberryPi2ModelBPin(pin);
+    }
+    else if (board.equals(RASPBERRY_PI_3_MODEL_B)) {
       return convert2RaspberryPi2ModelBPin(pin);
     }
     return convert2RaspberryPiModelBPin(pin);
   }
 
   private Object convert2RaspberryPi2ModelBPin(int pin) {
-    switch(pin) {
+    switch (pin) {
       case 3:
         return RaspiPin.GPIO_08;
       case 5:
@@ -134,7 +129,7 @@ public class RaspiGPIOServiceImpl {
   }
 
   private Object convert2RaspberryPiModelBPin(int pin) {
-    switch(pin) {
+    switch (pin) {
       case 3:
         return RaspiPin.GPIO_08;
       case 5:
